5891aea5162e3c8ac70c0728a1f8c31f9c8acaa8
hakimamarullah <hakimamarullah@gmail.com>
2023-07-21 08:48:32 +0700

    Initial commit

---
+++ src/main/java/com/juaracoding/haspringbootrestapi/HaSpringBootRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.juaracoding.haspringbootrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaSpringBootRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaSpringBootRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juaracoding/haspringbootrestapi/HaSpringBootRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juaracoding.haspringbootrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaSpringBootRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
